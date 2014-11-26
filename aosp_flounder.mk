74e93510d70a6e9a1dd884f9ca3d50fce6b76b91
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 19:31:34 +0900

    Remove the setting for LTE

--- aosp_flounder.mk
+++ aosp_flounder.mk
@@ -21,19 +21,13 @@
 # lines, aosp and flounder, hence its name.
 #
 
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-        rild \
-        Launcher3
-
-PRODUCT_PROPERTY_OVERRIDES := \
-        net.dns1=8.8.8.8 \
-        net.dns2=8.8.4.4
+#PRODUCT_PACKAGES += \
+#        rild
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/htc/flounder/product.mk)
-$(call inherit-product, device/htc/flounder/device-lte.mk)
-$(call inherit-product-if-exists, vendor/htc/flounder_lte/device-vendor.mk)
+#$(call inherit-product, device/htc/flounder/device-lte.mk)
+#$(call inherit-product-if-exists, vendor/htc/flounder_lte/device-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_NAME := aosp_flounder
